5462776058394c185745d93adf644ef5c640f9a3
chen <chen@qq.com>
2023-08-18 09:06:24 +0800

    我就瞎写了几个东西,别看 进度0%

--- day22/src/main/java/com/chen/Demo.java
+++ day22/src/main/java/com/chen/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
 
     public String say(String s){
         System.out.println("hello " + s);
+        System.out.println("hello1"+2);
         return "hello " + s;
+
     }
 
 }
